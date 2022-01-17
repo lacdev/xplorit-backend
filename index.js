bcda494b88aa939420eb8543912ee9a51e88bf48
German <german.mtz@protonmail.com>
2022-01-16 21:18:14 -0600

    add test route for server listening

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ dbConnect(process.env)
     })
   })
   .catch((error) => console.log(error))
+
+app.get('/test', (req, res) =>
+  res.send('Hello from the server. This is a test and should be ignored.')
+)
