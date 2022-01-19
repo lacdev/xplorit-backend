43d4f17614745723cb7b911e9c0847cdfe6ae4ea
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-01-19 17:23:08 -0600

    add user controller async functions

--- src/usecases/user.usecase.js
+++ src/usecases/user.usecase.js
@@ -4,6 +4,13 @@ const getAllUsers = async () => await User.find()
 
 const getSingleUser = async (id) => await User.findById(id)
 
+const createSingleUser = async (user) => await User.create(user)
+
+const updateSingleUser = async (id, body) =>
+  await User.findByIdAndUpdate(id, body)
+
+const deleteSingleUser = async (id) => await User.findByIdAndDelete(id)
+
 const getLikesMadeByUser = async (id) => await User.findById(id)
 
 const getReviewsMadeByUser = async (id) => await User.findById(id)
@@ -19,4 +26,7 @@ export {
   getReviewsMadeByUser,
   getPlacesCreatedByUser,
   getRoutesCreatedByUser,
+  updateSingleUser,
+  deleteSingleUser,
+  createSingleUser,
 }
