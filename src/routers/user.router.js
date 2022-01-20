8c7490ac373cca49a62a24879a7ce7eaeb30a55e
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-01-19 19:10:49 -0600

    add user and userOps router

---
+++ src/routers/user.router.js
@@ -0,0 +1,18 @@
+import * as express from 'express'
+import * as userController from 'controllers/user.controller'
+import * as opsController from 'controllers/userops.controller'
+
+const router = express.Router()
+
+//User controllers
+router.get('/', userController.getUsers)
+router.post('/', userController.saveUser)
+router.get('/:id', userController.getUser)
+router.patch('/:id', userController.updateUser)
+router.delete('/:id', userController.deleteUser)
+
+//User Ops controllers
+router.get('/:id/likes', opsController.getLikesByUser)
+router.get('/:id/reviews', opsController.getReviewsByUser)
+router.get('/:id/places', opsController.getPlacesByUser)
+router.get('/:id/routes', opsController.getRoutesByUser)
