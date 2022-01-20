e967dce9298154aac6f0ea5c8f6f60363ec07848
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-01-19 20:53:35 -0600

    commit problem with routers on server level to fix

--- src/lib/server.js
+++ src/lib/server.js
@@ -2,11 +2,14 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import { router as userRoutes } from 'routers/user.router'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan('combined'))
+
+app.use('/users', userRoutes)
 
 export default app
