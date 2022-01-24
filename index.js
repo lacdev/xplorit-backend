46662b3bd71145ee096adaca98eb20171238fd30
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-01-23 22:24:10 -0600

    add ApiError class with initial error handlers also add ApiErrorHandler Middleware for controllers.

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ import dbConnect from './src/lib/db.js'
 
 dotenv.config()
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080 || 9090
 
 dbConnect(process.env)
   .then(() => {
     console.log('DB Connected')
     app.listen(port, () => {
-      console.log(`Server is up and listening on port: ${port}`)
+      console.log(`Server is up and listening on port ${port}`)
     })
   })
   .catch((error) => console.log(error))
--- src/controllers/usersControllers/saveUser.js
+++ src/controllers/usersControllers/saveUser.js
@@ -1,9 +1,25 @@
 import { createSingleUser } from '../../usecases/userUsecases/createSingleUser.js'
 import { hashPassword } from '../../lib/bcrypt.js'
+import { ApiError } from '../../errors/ApiError.js'
 
-const saveUser = async (req, res) => {
+const saveUser = async (req, res, next) => {
   try {
     const { username, password, email } = req.body
+
+    if (!email) {
+      next(ApiError.badRequest('email is required and must be non blank'))
+      return
+    }
+
+    if (!password) {
+      next(ApiError.badRequest('password is required and must be non blank'))
+      return
+    }
+
+    if (!username) {
+      next(ApiError.badRequest('username is required and must be non blank'))
+      return
+    }
 
     const hashedPassword = await hashPassword(password)
 
@@ -22,13 +38,14 @@ const saveUser = async (req, res) => {
     }
   } catch (err) {
     console.error(err)
-    res.json({
-      message: 'failure',
-      error: {
-        description: 'Bad Request',
-        statusCode: 400,
-      },
-    })
+    next({})
+    // res.json({
+    //   message: 'failure',
+    //   error: {
+    //     description: 'Bad Request',
+    //     statusCode: 400,
+    //   },
+    // })
   }
 }
 
