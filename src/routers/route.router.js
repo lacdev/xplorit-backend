82870a082dc36a77f9cfe5bc593bd595bdeae998
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-01-28 00:51:44 -0600

    add route creation validation first iteration

--- src/routers/route.router.js
+++ src/routers/route.router.js
@@ -4,13 +4,14 @@ import { getRoute } from '../controllers/routesControllers/getRoute.js'
 import { saveRoute } from '../controllers/routesControllers/saveRoute.js'
 import { updateRoute } from '../controllers/routesControllers/updateRoute.js'
 import { deleteRoute } from '../controllers/routesControllers/deleteRoute.js'
+import { validateRouteCreation } from '../validators/routesValidators/saveRouteValidation.js'
 
 const router = express.Router()
 
 //Routes controller
 router.get('/', getRoutes)
 router.get('/:routeId', getRoute)
-router.post('/', saveRoute)
+router.post('/', validateRouteCreation, saveRoute)
 router.patch('/:routeId', updateRoute)
 router.delete('/:routeId', deleteRoute)
 
