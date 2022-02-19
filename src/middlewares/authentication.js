53825180ee6c741bb3d8854a9d6a483275f6c000
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-02-19 00:00:01 -0600

    add route authentication flow example

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -8,11 +8,21 @@ const verifyToken = async (req, res, next) => {
 
     const token = req.header('authorization')
 
+    // const tokenExtracted = token.split(' ')[1]
+
+    // console.log('token extracted', tokenExtracted)
+
+    // if (!tokenExtracted) {
+    //   next(ApiError.unauthorized('Unathorized Access. Token not provided.'))
+    // }
+
     if (!token) {
       next(ApiError.unauthorized('Unathorized Access. Token not provided.'))
     }
 
     const decoded = jwt.verify(token, SECRET)
+
+    console.log('token decoded object', decoded)
 
     if (!decoded) {
       next(
