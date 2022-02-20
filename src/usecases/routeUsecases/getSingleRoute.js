6c74ae8ac173df8ce34eff640e3c5afba18d22bf
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-02-20 00:39:02 -0600

    add populate on getPlace by Id

--- src/usecases/routeUsecases/getSingleRoute.js
+++ src/usecases/routeUsecases/getSingleRoute.js
@@ -2,6 +2,10 @@ import { Route } from '../../models/route.model.js'
 
 // let projection = { _id: 1, username: 1, avatar: 1, coverPhoto: 1, createdAt: 1 }
 
-const getSingleRoute = async (query) => await Route.findOne(query)
+const getSingleRoute = async (query) =>
+  await Route.findOne(query).populate({
+    path: 'ownerId',
+    select: 'username avatar',
+  })
 
 export { getSingleRoute }
