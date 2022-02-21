1d46784171a583a9bbdbe90fa6f811ff5fc37ef6
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-02-20 19:35:00 -0600

    add cover avatar users/me validation with authentication

--- src/controllers/usersControllers/updateAvatar.js
+++ src/controllers/usersControllers/updateAvatar.js
@@ -3,17 +3,17 @@ import { ApiError } from '../../errors/ApiError.js'
 
 const updateAvatar = async (req, res, next) => {
   try {
-    const { userId } = req.params
+    // const { userId } = req.params
     const { avatar } = req.body
 
-    // const { id } = req.user
+    const { id } = req.user
 
     //Validate payload equals to the user in the database they need to match.
     //Otherwise throw an error.
 
     // const foundUser = await getSingleUser({ _id: id })
 
-    const updatedUser = await updateSingleUser(userId, {
+    const updatedUser = await updateSingleUser(id, {
       avatar: avatar,
     })
 
