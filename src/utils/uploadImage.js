db82704e2b1a3e8c11871eaa5527e6a1c4d71043
lacdev <56662567+lacdev@users.noreply.github.com>
2023-02-10 00:13:32 -0600

    update gcp bucket storage env variables

--- src/utils/uploadImage.js
+++ src/utils/uploadImage.js
@@ -3,21 +3,7 @@ import { Storage } from '@google-cloud/storage'
 import { v4 } from 'uuid'
 
 const storage = new Storage({
-  // keyFile: process.env.GCP_SECRET,
-  projectId: process.env.GCP_PROJECT_ID,
-  credentials: {
-    type: process.env.GCP_TYPE,
-    projectId: process.env.GCP_PROJECT_ID,
-    project_id: process.env.GCP_PROJECT_ID,
-    private_key_id: process.env.GCP_PRIVATE_KEY_ID,
-    private_key: process.env.GCP_PRIVATE_KEY,
-    client_email: process.env.GCP_CLIENT_EMAIL,
-    client_id: process.env.GCP_CLIENT_ID,
-    auth_uri: process.env.GCP_AUTH_URI,
-    token_uri: process.env.GCP_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.GCP_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.GCP_CLIENT_X509_CERT_URL,
-  },
+  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 })
 
 const bucket = storage.bucket(variables.GCP_BUCKET) //Name of the bucket
